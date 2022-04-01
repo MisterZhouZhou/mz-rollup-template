39d76c5873e5d4c6f3750c95b7be4f85c980c21a
MisterZhou <16619930394@163.com>
2022-04-01 16:33:02 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2'
 import serve from 'rollup-plugin-serve'
 import replace from '@rollup/plugin-replace'
+import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import { resolve } from 'path'
 
@@ -22,6 +23,7 @@ export default {
     nodeResolve({  // 查找和打包node_modules中的第三方模块
       extensions: ['.js', '.ts']
     }),
+    commonjs(), // commonjs转es6,有些模块无法通过import导入
     typescript({ // 解析TypeScript
       tsconfig: resolve(__dirname, 'tsconfig.json')
     }),
